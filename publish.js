adfb9eeb6a792b02d79a0c4df8479108a29d2b04
More-Gowri <35288860+More-Gowri@users.noreply.github.com>
2018-01-10 13:07:41 +0530

    Add files via upload

---
+++ publish.js
@@ -0,0 +1,52 @@
+var zipFolder = require('zip-folder');
+var path = require('path');
+var fs = require('fs');
+var request = require('request');
+
+var rootFolder = path.resolve('.');
+var zipPath = path.resolve(rootFolder, '../eltbot.zip');
+var kuduApi = 'https://eltbot.scm.azurewebsites.net/api/zip/site/wwwroot';
+var userName = '$eltbot';
+var password = '1iQbkA5t3v8j8vYw9CPgv7QlfeCDMbdsoGsu7lAbsfl7LuToqkwn3XBpXgBM';
+
+function uploadZip(callback) {
+  fs.createReadStream(zipPath).pipe(request.put(kuduApi, {
+    auth: {
+      username: userName,
+      password: password,
+      sendImmediately: true
+    },
+    headers: {
+      "Content-Type": "applicaton/zip"
+    }
+  }))
+  .on('response', function(resp){
+    if (resp.statusCode >= 200 && resp.statusCode < 300) {
+      fs.unlink(zipPath);
+      callback(null);
+    } else if (resp.statusCode >= 400) {
+      callback(resp);
+    }
+  })
+  .on('error', function(err) {
+    callback(err)
+  });
+}
+
+function publish(callback) {
+  zipFolder(rootFolder, zipPath, function(err) {
+    if (!err) {
+      uploadZip(callback);
+    } else {
+      callback(err);
+    }
+  })
+}
+
+publish(function(err) {
+  if (!err) {
+    console.log('eltbot publish');
+  } else {
+    console.error('failed to publish eltbot', err);
+  }
+});
